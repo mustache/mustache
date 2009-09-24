a2fb7e6eb45ff7a76d9a09e0d5c015ec7d7b9bc3
Chris Wanstrath <chris@ozmm.org>
2009-09-24 01:58:53 -0700

    add comments examples

---
+++ examples/comments.rb
@@ -0,0 +1,14 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
+require 'rtemplate'
+
+class Comments < RTemplate
+  self.path = File.dirname(__FILE__)
+
+  def title
+    "A Comedy of Errors"
+  end
+end
+
+if $0 == __FILE__
+  puts Comments.to_html
+end

4b682f395780a381a16c74d67a2dcc0a3ce568c4
Chris Wanstrath <chris@ozmm.org>
2009-10-05 15:30:36 -0700

    rdoc friendly sinatra docs

--- lib/mustache/sinatra.rb
+++ lib/mustache/sinatra.rb
@@ -1,29 +1,27 @@
-=begin
-Support for Mustache in your Sinatra app.
-
-  require 'mustache/sinatra'
-
-  class App < Sinatra::Base
-    helpers Mustache::Sinatra
-
-    get '/stats' do
-      mustache :stats
-    end
-  end
-
-If a `Views::Stats` class exists in the above example,
-Mustache will try to instantiate and use it for the rendering.
-
-If no `Views::Stats` class exists Mustache will render the template
-file directly.
-
-You can indeed use layouts with this library. Where you'd normally
-<%= yield %> you instead {{{yield}}} - the body of the subview is
-set to the `yield` variable and made available to you.
-=end
 require 'mustache'
 
 class Mustache
+  # Support for Mustache in your Sinatra app.
+  #
+  #   require 'mustache/sinatra'
+  #
+  #   class App < Sinatra::Base
+  #     helpers Mustache::Sinatra
+  #
+  #     get '/stats' do
+  #       mustache :stats
+  #     end
+  #   end
+  #
+  # If a `Views::Stats` class exists in the above example,
+  # Mustache will try to instantiate and use it for the rendering.
+  #
+  # If no `Views::Stats` class exists Mustache will render the template
+  # file directly.
+  #
+  # You can indeed use layouts with this library. Where you'd normally
+  # <%= yield %> you instead {{{yield}}} - the body of the subview is
+  # set to the `yield` variable and made available to you.
   module Sinatra
     # Call this in your Sinatra routes.
     def mustache(template, options={}, locals={})

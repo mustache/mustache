be9b7bd52a74dae122e94ea64674293abf2da737
Chris Wanstrath <chris@ozmm.org>
2009-10-08 19:31:30 -0700

    Improved Sinatra support, but you'll need to update your app.
    Notably we added the `mustaches` and `namespace` settings. You can use
    `mustaches` to tell Mustache where Ruby view files are located, e.g.
    
        set :mustaches, "path/to/mustache/views"
    
    The `namespace` option is used for telling Mustache which namespace
    the Views classes all live under. If you have a view named
    `Hurl::Views::Stats`, you'll want to set `namespace` to Hurl:
    
        set :namespace, Hurl
    
    and put the class into a file named `stats.rb` that lives under the
    `mustaches` setting:
    
        $ cat path/to/mustache/views/stats.rb
        module Hurl
          module Views
            class Stats < Mustache
              # blah
            end
          end
        end
    
    The `mustaches` setting defaults to the Sinatra `views` setting and
    the `namespace` setting defaults to `Object`.

--- lib/mustache/sinatra.rb
+++ lib/mustache/sinatra.rb
@@ -7,15 +7,27 @@ class Mustache
   #   require 'mustache/sinatra'
   #
   #   class App < Sinatra::Base
-  #     helpers Mustache::Sinatra
+  #     # Should be the path to your .mustache template files.
+  #     set :views, "path/to/mustache/templates"
+  #
+  #     # Should be the path to your .rb Mustache view files.
+  #     # Only needed if different from the `views` setting
+  #     set :mustaches, "path/to/mustache/views"
+  #
+  #     # This tells Mustache where to look for the Views modules,
+  #     # under which your View classes should live. By default it's
+  #     # Object. That is, for an :index view Mustache will expect
+  #     # Views::Index. In this example, we're telling Mustache to look
+  #     # for index at App::Views::Index.
+  #     set :namespace, App
   #
   #     get '/stats' do
   #       mustache :stats
   #     end
   #   end
   #
-  # If a `Views::Stats` class exists in the above example,
-  # Mustache will try to instantiate and use it for the rendering.
+  # As noted above, Mustache will look for `App::Views::Index` when
+  # `mustache :index` is called.
   #
   # If no `Views::Stats` class exists Mustache will render the template
   # file directly.
@@ -34,16 +46,37 @@ class Mustache
     def render_mustache(template, data, options, locals, &block)
       name = Mustache.classify(template.to_s)
 
-      if defined?(Views) && Views.const_defined?(name)
-        instance = Views.const_get(name).new
+      # This is a horrible hack but we need it to know under which namespace
+      # Views is located. If you have Haystack::Views, namespace should be
+      # set to Haystack.
+      namespace = self.class.namespace
+
+      if namespace.const_defined?(:Views) && namespace::Views.const_defined?(name)
+        # First try to find the existing view,
+        # e.g. Haystack::Views::Index
+        klass = namespace::Views.const_get(name)
+
+      elsif File.exists?(file = "#{self.class.mustaches}/#{template}.rb")
+        # Couldn't find it - try to require the file if it exists, then
+        # load in the view.
+        require "#{file}".chomp('.rb')
+        klass = namespace::Views.const_get(name)
+
       else
-        instance = Mustache.new
+        # Still nothing. Use the stache.
+        klass = Mustache
+
       end
 
+      # Create a new instance for playing with
+      instance = klass.new
+
+      # Copy instance variables set in Sinatra to the view
       instance_variables.each do |name|
         instance.instance_variable_set(name, instance_variable_get(name))
       end
 
+      # Locals get added to the view's context
       locals.each do |local, value|
         instance[local] = value
       end
@@ -58,4 +91,6 @@ class Mustache
   end
 end
 
-Sinatra.helpers Mustache::Sinatra
+Sinatra::Base.helpers Mustache::Sinatra
+Sinatra::Base.set :mustaches, Sinatra::Base.views
+Sinatra::Base.set :namespace, Object

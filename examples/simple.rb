3111f421de6664b6b7f54e6bef7d76b63bca3552
Chris Wanstrath <chris@ozmm.org>
2010-03-11 14:27:31 -0800

    real example

---
+++ examples/simple.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
+require 'mustache'
+
+class Simple < Mustache
+  self.path = File.dirname(__FILE__)
+
+  def name
+    "Chris"
+  end
+
+  def value
+    10_000
+  end
+
+  def taxed_value
+    value - (value * 0.4)
+  end
+
+  def in_ca
+    true
+  end
+end
+
+puts Simple.render if $0 == __FILE__

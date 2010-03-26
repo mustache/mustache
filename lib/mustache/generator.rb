e32f2e20a6f21366362504202d24d12734bac6ae
Chris Wanstrath <chris@ozmm.org>
2010-03-26 10:52:02 -0700

    Some Generator comments

--- lib/mustache/generator.rb
+++ lib/mustache/generator.rb
@@ -1,13 +1,74 @@
 class Mustache
+  # The Generator is in charge of taking an array of Mustache tokens,
+  # usually assembled by the Parser, and generating an interpolatable
+  # Ruby string. This string is considered the "compiled" template
+  # because at that point we're relying on Ruby to do the parsing and
+  # run our code.
+  #
+  # For example, let's take this template:
+  #
+  #   Hi {{thing}}!
+  #
+  # If we run this through the Parser we'll get these tokens:
+  #
+  #   [:multi,
+  #     [:static, "Hi "],
+  #     [:mustache, :etag, "thing"],
+  #     [:static, "!\n"]]
+  #
+  # Now let's hand that to the Generator:
+  #
+  # >> puts Mustache::Generator.new.compile(tokens)
+  # "Hi #{CGI.escapeHTML(ctx[:thing].to_s)}!\n"
   class Generator
+    # Options are unused for now but may become useful in the future.
     def initialize(options = {})
       @options = options
     end
 
+    # Given an array of tokens, returns an interpolatable Ruby string.
     def compile(exp)
       "\"#{compile!(exp)}\""
     end
 
+    # Given an array of tokens, converts them into Ruby code. In
+    # particular there are three types of expressions we are concerned
+    # with:
+    #
+    #   :multi
+    #     Mixed bag of :static, :mustache, and whatever.
+    #
+    #   :static
+    #     Normal HTML, the stuff outside of {{mustaches}}.
+    #
+    #   :mustache
+    #     Any Mustache tag, from sections to partials.
+    #
+    # To give you an idea of what you'll be dealing with take this
+    # template:
+    #
+    #   Hello {{name}}
+    #   You have just won ${{value}}!
+    #   {{#in_ca}}
+    #   Well, ${{taxed_value}}, after taxes.
+    #   {{/in_ca}}
+    #
+    # If we run this through the Parser, we'll get back this array of
+    # tokens:
+    #
+    #   [:multi,
+    #    [:static, "Hello "],
+    #    [:mustache, :etag, "name"],
+    #    [:static, "\nYou have just won $"],
+    #    [:mustache, :etag, "value"],
+    #    [:static, "!\n"],
+    #    [:mustache,
+    #     :section,
+    #     "in_ca",
+    #     [:multi,
+    #      [:static, "Well, $"],
+    #      [:mustache, :etag, "taxed_value"],
+    #      [:static, ", after taxes.\n"]]]]
     def compile!(exp)
       case exp.first
       when :multi
@@ -21,8 +82,15 @@ class Mustache
       end
     end
 
+    # Callback fired when the compiler finds a section token. We're
+    # passed the section name and the array of tokens.
     def on_section(name, content)
+      # Convert the tokenized content of this section into a Ruby
+      # string we can use.
       code = compile(content)
+
+      # Compile the Ruby for this section now that we know what's
+      # inside the section.
       ev(<<-compiled)
       if v = ctx[#{name.to_sym.inspect}]
         if v == true
@@ -35,14 +103,19 @@ class Mustache
       compiled
     end
 
+    # Fired when the compiler finds a partial. We want to return code
+    # which calls a partial at runtime instead of expanding and
+    # including the partial's body to allow for recursive partials.
     def on_partial(name)
       ev("ctx.partial(#{name.to_sym.inspect})")
     end
 
+    # An unescaped tag.
     def on_utag(name)
       ev("ctx[#{name.to_sym.inspect}]")
     end
 
+    # An escaped tag.
     def on_etag(name)
       ev("CGI.escapeHTML(ctx[#{name.to_sym.inspect}].to_s)")
     end
